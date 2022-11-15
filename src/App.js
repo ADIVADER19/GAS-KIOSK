988587b65163a7318ebe8f569016a325e2712d5a
ADIVADER19 <advaitnurani@gmail.com>
2022-11-15 11:15:56 +0530

    gui update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,30 @@ import './App.css';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {useEffect,useState}  from 'react';
-
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import axios from 'axios';
 function App() {
   const [dat,setDat]=useState(0);
-  const [co,useCo]=useState(0);
-  const [smoke,useSmoke]=useState(0);
-     
+  const theme = createTheme();
 
-  
-  // useEffect(()=>
-  //   {
-  //     useCo(200);
-  //     if ((co>=65) || (smoke>=45) || (lpg>=550))
-  //     {
-  //       noti();
-  //     }
-  //   },[]);
+  theme.typography.h3 = {
+    fontSize: '1.2rem',
+    '@media (min-width:600px)': {
+      fontSize: '1.5rem',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '2.4rem',
+    },
+  };
+
+
   return (
 
     <div className="App">
       <div className="container">
-        <Typography className="title" variant="h1">GAS KIOSK </Typography>
-        <iframe width="450" height="260" style={{border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1884399/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+        <ThemeProvider theme={theme}><Typography className="title" variant="h3" align="center">GAS KIOSK </Typography></ThemeProvider>
+        <br/>
+        <iframe width="450" height="260" style={{backgroundColor:'#000000',border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1884399/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
       </div>
     </div>
   );
